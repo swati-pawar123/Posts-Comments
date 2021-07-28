fb4e1624ecab52400ac527224321045c5fc53526
swati-pawar123 <spawar2097@gmail.com>
2021-07-28 18:34:21 +0530

    Authentication added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
 
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.paginate(page: params[:page], per_page: 3).order("created_at DESC")
+    #@posts = Post.all.order("created_at DESC")
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :posts do
     resources :comments
   end
